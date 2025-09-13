27090a62331f4b20e35b2d56243853778aa89fb6
seon19 <woosun1745@naver.com>
2025-09-14 07:38:03 +0900

    add spring boot project

---
+++ src/main/java/com/sp/app/WitchApplication.java
@@ -0,0 +1,13 @@
+package com.sp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WitchApplication.class, args);
+	}
+
+}
